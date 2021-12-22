255377344e3a3bbafba8a2d342b8bf040d2b77ba
jayanta2018 <jayanta.saha@hee.nhs.uk>
2021-12-22 12:18:50 +0000

    chore: add log for getting clear picture

--- application/src/main/java/uk/nhs/hee/tis/revalidation/service/GmcDoctorNightlySyncService.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/service/GmcDoctorNightlySyncService.java
@@ -1,10 +1,12 @@
 package uk.nhs.hee.tis.revalidation.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import uk.nhs.hee.tis.revalidation.messages.publisher.GmcDoctorsForDbSyncStartPublisher;
 
+@Slf4j
 @Service
 public class GmcDoctorNightlySyncService {
 
@@ -24,7 +26,9 @@ public class GmcDoctorNightlySyncService {
   @SchedulerLock(name = "GmcNightlySyncJob")
   public void startNightlyGmcDoctorSync() {
     this.doctorsForDBService.hideAllDoctors();
+    log.info("All doctors are hidden now");
     this.gmcDoctorsForDbSyncStartPublisher.publishNightlySyncStartMessage();
+    log.info("Start message has been sent to start gmc sync");
   }
 
 }
