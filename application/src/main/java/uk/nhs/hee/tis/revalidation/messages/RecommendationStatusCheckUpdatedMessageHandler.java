de24425d80213d5077110130ef7da46aeb5e9335
tisadmincw <tisadmincw@N2810.xhee.nhs.uk>
2021-12-22 14:55:39 +0000

    fix: update all statuses on hourly check

--- application/src/main/java/uk/nhs/hee/tis/revalidation/messages/RecommendationStatusCheckUpdatedMessageHandler.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/messages/RecommendationStatusCheckUpdatedMessageHandler.java
@@ -72,8 +72,7 @@ public class RecommendationStatusCheckUpdatedMessageHandler {
         .findById(gmcReferenceNumber);
 
     //update the tis status in doctorsfordb to "complete"
-    if (optionalDoctorsForDB.isPresent() && (APPROVED.equals(outcome) || REJECTED
-        .equals(outcome))) {
+    if (optionalDoctorsForDB.isPresent()) {
       final var doctorsForDB = optionalDoctorsForDB.get();
       doctorsForDB.setDoctorStatus(
           recommendationService.getRecommendationStatusForTrainee(gmcReferenceNumber));
@@ -92,18 +91,18 @@ public class RecommendationStatusCheckUpdatedMessageHandler {
       log.warn("Ignoring an update to an unknown Recommendation: {}", recommendationId);
       return;
     }
+    Recommendation recommendation = optionalRecommendation.get();
+    recommendation.setOutcome(recommendationGmcOutcome);
     if (APPROVED.equals(recommendationGmcOutcome) || REJECTED.equals(recommendationGmcOutcome)) {
-      Recommendation recommendation = optionalRecommendation.get();
-      recommendation.setOutcome(recommendationGmcOutcome);
       recommendation.setRecommendationStatus(COMPLETED);
-      recommendationRepository.save(recommendation);
+    }
+    recommendationRepository.save(recommendation);
 
-      // Find the snapshot from snapshot repository and check the gmcRecommendationId.
-      // If it exists then don't save snapshot else save it
-      if (!doesSnapshotRecommendationExist(recommendation.getGmcNumber(), gmcRecommendationId)) {
-        recommendation.setGmcRevalidationId(gmcRecommendationId);
-        snapshotService.saveRecommendationToSnapshot(recommendation);
-      }
+    // Find the snapshot from snapshot repository and check the gmcRecommendationId.
+    // If it exists then don't save snapshot else save it
+    if (!doesSnapshotRecommendationExist(recommendation.getGmcNumber(), gmcRecommendationId)) {
+      recommendation.setGmcRevalidationId(gmcRecommendationId);
+      snapshotService.saveRecommendationToSnapshot(recommendation);
     }
   }
 
