11084727bc6f71399c71e109d7107be6d16f8dc3
tisadmincw <tisadmincw@N2810.xhee.nhs.uk>
2022-03-28 12:18:33 +0100

    chore: clean up old invalid snapshots

--- application/src/main/java/uk/nhs/hee/tis/revalidation/changelog/RemoveAllInvalidRecommendationsChangeLog.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/changelog/RemoveAllInvalidRecommendationsChangeLog.java
@@ -5,6 +5,7 @@ import com.github.cloudyrock.mongock.ChangeSet;
 import static uk.nhs.hee.tis.revalidation.entity.RecommendationStatus.SUBMITTED_TO_GMC;
 
 import uk.nhs.hee.tis.revalidation.repository.RecommendationRepository;
+import uk.nhs.hee.tis.revalidation.repository.SnapshotRepository;
 
 @ChangeLog(order = "004")
 public class RemoveAllInvalidRecommendationsChangeLog {
@@ -18,6 +19,20 @@ public class RemoveAllInvalidRecommendationsChangeLog {
         recommendation -> {
           if (recommendation.getGmcRevalidationId() == null) {
             recommendationRepository.delete(recommendation);
+          }
+        }
+    );
+  }
+
+  @ChangeSet(order = "002", id = "removeAllInvalidSnapshots", author = "")
+  public void removeAllInvalidSnapshots(
+      SnapshotRepository snapshotRepository
+  ) {
+    snapshotRepository
+        .findAll().forEach(
+        snapshot -> {
+          if (snapshot.getRevalidation().getGmcRecommendationId() == null) {
+            snapshotRepository.delete(snapshot);
           }
         }
     );
--- application/src/test/java/uk/nhs/hee/tis/revalidation/changelog/RemoveAllInvalidRecommendationsChangeLogTest.java
+++ application/src/test/java/uk/nhs/hee/tis/revalidation/changelog/RemoveAllInvalidRecommendationsChangeLogTest.java
@@ -7,13 +7,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.nhs.hee.tis.revalidation.entity.Recommendation;
+import uk.nhs.hee.tis.revalidation.entity.Snapshot;
 import uk.nhs.hee.tis.revalidation.repository.RecommendationRepository;
-import static uk.nhs.hee.tis.revalidation.entity.RecommendationStatus.SUBMITTED_TO_GMC;
+import uk.nhs.hee.tis.revalidation.repository.SnapshotRepository;
+import uk.nhs.hee.tis.revalidation.entity.SnapshotRevalidation;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.nhs.hee.tis.revalidation.entity.RecommendationStatus.SUBMITTED_TO_GMC;
 
 @ExtendWith(MockitoExtension.class)
 class RemoveAllInvalidRecommendationsChangeLogTest {
@@ -24,11 +27,16 @@ class RemoveAllInvalidRecommendationsChangeLogTest {
   @Mock
   RecommendationRepository recommendationRepository;
 
+  @Mock
+  SnapshotRepository snapshotRepository;
+
   private static final String RECOMMENDATION_ID = "123";
   private static final String GMC_REVALIDATION_ID = "456";
 
   private Recommendation validRecommendation = buildValidTestRecommendation();
   private Recommendation invalidRecommendation = buildInvalidTestRecommendation();
+  private Snapshot validSnapshot = buildValidTestSnapshot();
+  private Snapshot invalidSnapshot = buildInvalidTestSnapshot();
 
   @Test
   void shouldNotDeleteRecommendationIfSubmittedWithGmcRevalidationId() {
@@ -48,6 +56,24 @@ class RemoveAllInvalidRecommendationsChangeLogTest {
     verify(recommendationRepository).delete(invalidRecommendation);
   }
 
+  @Test
+  void shouldNotDeleteSnapshotIfSubmittedWithGmcRecommendationId() {
+    when(snapshotRepository.findAll()).thenReturn(
+        List.of(validSnapshot)
+    );
+    changeLog.removeAllInvalidSnapshots(snapshotRepository);
+    verify(snapshotRepository, never()).delete(validSnapshot);
+  }
+
+  @Test
+  void shouldDeleteSnapshotIfSubmittedWithNoGmcRecommendationId() {
+    when(snapshotRepository.findAll()).thenReturn(
+        List.of(invalidSnapshot)
+    );
+    changeLog.removeAllInvalidSnapshots(snapshotRepository);
+    verify(snapshotRepository).delete(invalidSnapshot);
+  }
+
   private Recommendation buildValidTestRecommendation() {
     return Recommendation.builder()
         .id(RECOMMENDATION_ID)
@@ -58,6 +84,24 @@ class RemoveAllInvalidRecommendationsChangeLogTest {
   private Recommendation buildInvalidTestRecommendation() {
     return Recommendation.builder()
         .id(RECOMMENDATION_ID)
+        .build();
+  }
+
+  private Snapshot buildValidTestSnapshot() {
+    return Snapshot.builder()
+        .id(RECOMMENDATION_ID)
+        .revalidation(
+            SnapshotRevalidation.builder().gmcRecommendationId(GMC_REVALIDATION_ID).build()
+        )
+        .build();
+  }
+
+  private Snapshot buildInvalidTestSnapshot() {
+    return Snapshot.builder()
+        .id(RECOMMENDATION_ID)
+        .revalidation(
+            SnapshotRevalidation.builder().build()
+        )
         .build();
   }
 
