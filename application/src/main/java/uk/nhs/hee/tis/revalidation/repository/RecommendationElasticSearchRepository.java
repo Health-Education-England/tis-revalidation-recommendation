391b2de1b223d761f5e52eb4d8f8c84b4c2b86a6
TISadminCW <TISadminCW@N3647.xhee.nhs.uk>
2022-05-13 10:23:22 +0100

    feat: enable search by programme name in search bar
    TIS21-276: Filter Recommendations with ES - Retrieve data from ES

--- application/src/main/java/uk/nhs/hee/tis/revalidation/repository/RecommendationElasticSearchRepository.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/repository/RecommendationElasticSearchRepository.java
@@ -34,13 +34,13 @@ public interface RecommendationElasticSearchRepository
     extends ElasticsearchRepository<RecommendationView, String> {
 
   @Query(
-    "{\"bool\":{\"filter\":[{\"match\":{\"underNotice\":\"YES\"}},{\"match\":{\"designatedBody\":\"?1\"}},{\"match\":{\"existsInGmc\":\"true\"}},{\"bool\":{\"should\":[{\"wildcard\":{\"doctorFirstName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"doctorLastName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"gmcReferenceNumber\":{\"value\":\"*?0*\"}}}]}}]}}"
-      )
+    "{\"bool\":{\"filter\":[{\"match\":{\"underNotice\":\"YES\"}},{\"match\":{\"designatedBody\":\"?1\"}},{\"match\":{\"existsInGmc\":\"true\"}},{\"bool\":{\"should\":[{\"wildcard\":{\"doctorFirstName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"doctorLastName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"gmcReferenceNumber\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"programmeName\":{\"value\":\"*?0*\"}}}]}}]}}"
+  )
   Page<RecommendationView> findByUnderNotice(final String searchQuery,
       final List<String> dbcs, final Pageable pageable);
 
   @Query(
-    "{\"bool\":{\"must_not\":{\"match\":{\"gmcReferenceNumber\":\"?2\"}},\"filter\":[{\"match\":{\"designatedBody\":\"?1\"}},{\"match\":{\"existsInGmc\":\"true\"}},{\"bool\":{\"should\":[{\"wildcard\":{\"doctorFirstName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"doctorLastName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"gmcReferenceNumber\":{\"value\":\"*?0*\"}}}]}}]}}"
+    "{\"bool\":{\"must_not\":{\"match\":{\"gmcReferenceNumber\":\"?2\"}},\"filter\":[{\"match\":{\"designatedBody\":\"?1\"}},{\"match\":{\"existsInGmc\":\"true\"}},{\"bool\":{\"should\":[{\"wildcard\":{\"doctorFirstName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"doctorLastName\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"gmcReferenceNumber\":{\"value\":\"*?0*\"}}},{\"wildcard\":{\"programmeName\":{\"value\":\"*?0*\"}}}]}}]}}"
   )
   Page<RecommendationView> findAll(final String searchQuery,
       final List<String> dbcs, List<String> hiddenGmcIds, final Pageable pageable);
