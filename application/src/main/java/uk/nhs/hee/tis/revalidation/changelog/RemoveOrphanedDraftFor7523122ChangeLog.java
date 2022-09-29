1000d4254ecd52dc9c6f3dc3e747299b5e32af56
TISadminCW <TISadminCW@N3647.xhee.nhs.uk>
2022-09-29 13:35:54 +0100

    chore: java friendly method name and tests
    TIS21-3528 Deletion of doctor's draft recommendation (GMC7523122) in reval under notice

--- application/src/main/java/uk/nhs/hee/tis/revalidation/changelog/RemoveOrphanedDraftFor7523122ChangeLog.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/changelog/RemoveOrphanedDraftFor7523122ChangeLog.java
@@ -7,12 +7,12 @@ import uk.nhs.hee.tis.revalidation.entity.Recommendation;
 import uk.nhs.hee.tis.revalidation.repository.RecommendationRepository;
 
 @ChangeLog(order = "005")
-public class RemoveOrphanedDraftFor7523122 {
+public class RemoveOrphanedDraftFor7523122ChangeLog {
 
   private static final String RECOMMENDATION_ID = "62d150a28492f85d3fca8dba";
 
   @ChangeSet(order="001", id = "removeInvalidRecommendationForDoctor7523122", author = "")
-  public void RemoveOrphanedDraftForDoctor7523122(
+  public void removeOrphanedDraftForDoctor7523122(
       RecommendationRepository recommendationRepository
   ) {
     Optional<Recommendation> recommendation = recommendationRepository.findById(
