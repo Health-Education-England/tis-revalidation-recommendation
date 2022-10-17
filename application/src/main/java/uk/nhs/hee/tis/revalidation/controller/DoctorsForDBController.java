9e22dc3bee0f6d0af593a6ce6de48eb10201fed5
TISadminCW <TISadminCW@N3647.xhee.nhs.uk>
2022-10-17 13:50:08 +0100

    chore: remove redundant required=true on request parameters
    TIS21-3554: Change existing query in ES to include GMC status as separate parameters

--- application/src/main/java/uk/nhs/hee/tis/revalidation/controller/DoctorsForDBController.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/controller/DoctorsForDBController.java
@@ -94,14 +94,14 @@ public class DoctorsForDBController {
       @ApiResponse(code = 200, message = "Trainee gmc all doctors data", response = TraineeSummaryDto.class)})
   @GetMapping
   public ResponseEntity<TraineeSummaryDto> getTraineeDoctorsInformation(
-      @RequestParam(name = SORT_COLUMN, defaultValue = SUBMISSION_DATE, required = false) final String sortColumn,
-      @RequestParam(name = SORT_ORDER, defaultValue = ASC, required = false) final String sortOrder,
-      @RequestParam(name = UNDER_NOTICE, defaultValue = UNDER_NOTICE_VALUE, required = false) final boolean underNotice,
-      @RequestParam(name = PAGE_NUMBER, defaultValue = PAGE_NUMBER_VALUE, required = false) final int pageNumber,
+      @RequestParam(name = SORT_COLUMN, defaultValue = SUBMISSION_DATE) final String sortColumn,
+      @RequestParam(name = SORT_ORDER, defaultValue = ASC) final String sortOrder,
+      @RequestParam(name = UNDER_NOTICE, defaultValue = UNDER_NOTICE_VALUE) final boolean underNotice,
+      @RequestParam(name = PAGE_NUMBER, defaultValue = PAGE_NUMBER_VALUE) final int pageNumber,
       @RequestParam(name = DESIGNATED_BODY_CODES, required = false) final List<String> dbcs,
-      @RequestParam(name = PROGRAMME_NAME_PARAM, defaultValue = EMPTY_STRING, required = false) final String programmeName,
-      @RequestParam(name = GMC_STATUS_PARAM, defaultValue = EMPTY_STRING, required = false) final String gmcStatus,
-      @RequestParam(name = SEARCH_QUERY, defaultValue = EMPTY_STRING, required = false) final String searchQuery) {
+      @RequestParam(name = PROGRAMME_NAME_PARAM, defaultValue = EMPTY_STRING) final String programmeName,
+      @RequestParam(name = GMC_STATUS_PARAM, defaultValue = EMPTY_STRING) final String gmcStatus,
+      @RequestParam(name = SEARCH_QUERY, defaultValue = EMPTY_STRING) final String searchQuery) {
     final var traineeRequestDTO = TraineeRequestDto.builder().sortColumn(sortColumn)
         .sortOrder(sortOrder).underNotice(underNotice).pageNumber(pageNumber).dbcs(dbcs)
         .searchQuery(searchQuery).programmeName(programmeName).gmcStatus(gmcStatus).build();
