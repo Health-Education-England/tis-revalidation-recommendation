e97f22df66646426097cd9a4d0c8db5e9d2513dc
Yafang Deng <hihilary@outlook.com>
2023-11-16 11:57:10 +0000

    fix:move @EnableMongoAuditing from main class to other file
    TIS21-5224

---
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/config/MongoDbConfig.java
@@ -0,0 +1,10 @@
+package uk.nhs.hee.tis.revalidation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoDbConfig {
+
+}
