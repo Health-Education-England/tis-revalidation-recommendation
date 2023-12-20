8a72d0c9a9230ff43b204802afe1b0a8208cf92f
Cai Willis <Cai.Willis@hee.nhs.uk>
2023-12-20 11:05:50 +0000

    fix: restore object mapper bean for use in sqs
    TIS21-5495: Remove config and Active profile TCS mapper for stage

--- application/src/main/java/uk/nhs/hee/tis/revalidation/RevalidationApplication.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/RevalidationApplication.java
@@ -21,6 +21,10 @@
 
 package uk.nhs.hee.tis.revalidation;
 
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -39,7 +43,13 @@ public class RevalidationApplication {
   public static void main(String[] args) {
     SpringApplication.run(RevalidationApplication.class, args);
   }
-
+  @Bean
+  public ObjectMapper objectMapper() {
+    final var mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    mapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
+    return mapper;
+  }
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
