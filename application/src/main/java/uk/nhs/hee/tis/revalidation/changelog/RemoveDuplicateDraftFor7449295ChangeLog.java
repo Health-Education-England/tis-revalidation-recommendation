ab3f2a3a330a82e87556710bfa7ca46cc4a3873c
NM2.7JBY9KP8 <cai.willis@nhs.net>
2025-09-25 11:51:30 +0100

    chore: remove duplicate draft recommendation for 7449295
    NO_TICKET:

---
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/changelog/RemoveDuplicateDraftFor7449295ChangeLog.java
@@ -0,0 +1,26 @@
+package uk.nhs.hee.tis.revalidation.changelog;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import java.util.Optional;
+import uk.nhs.hee.tis.revalidation.entity.Recommendation;
+import uk.nhs.hee.tis.revalidation.repository.RecommendationRepository;
+
+@ChangeLog(order = "009")
+public class RemoveDuplicateDraftFor7449295ChangeLog {
+
+  private static final String RECOMMENDATION_ID = "65b79baae6d8ff00033dd1b7";
+
+  @ChangeSet(order="001", id = "removeInvalidRecommendationForDoctor7449295", author = "")
+  public void removeDuplicateDraftFor7449295(
+      RecommendationRepository recommendationRepository
+  ) {
+    Optional<Recommendation> recommendation = recommendationRepository.findById(
+        RECOMMENDATION_ID
+    );
+    if(recommendation.isPresent()) {
+      recommendationRepository.delete(recommendation.get());
+    }
+  }
+
+}
---
+++ application/src/test/java/uk/nhs/hee/tis/revalidation/changelog/RemoveDuplicateDraftFor7449295ChangeLogTest.java
@@ -0,0 +1,61 @@
+package uk.nhs.hee.tis.revalidation.changelog;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.nhs.hee.tis.revalidation.entity.Recommendation;
+import uk.nhs.hee.tis.revalidation.repository.RecommendationRepository;
+
+@ExtendWith(MockitoExtension.class)
+class RemoveDuplicateDraftFor7449295ChangeLogTest {
+
+  @InjectMocks
+  RemoveDuplicateDraftFor7449295ChangeLog changeLog;
+
+  @Mock
+  RecommendationRepository recommendationRepository;
+
+  private static final String RECOMMENDATION_ID = "65b79baae6d8ff00033dd1b7";
+
+  @BeforeEach
+  void setup() {
+    changeLog = new RemoveDuplicateDraftFor7449295ChangeLog();
+  }
+
+  @Test
+  void shouldRemoveRecommendationWithGivenIdIfPresent() {
+    var testRecommendation = buildTestRecommendation();
+
+    when(recommendationRepository.findById(RECOMMENDATION_ID))
+        .thenReturn(Optional.of(testRecommendation));
+
+    changeLog.removeDuplicateDraftFor7449295(recommendationRepository);
+
+    verify(recommendationRepository).delete(testRecommendation);
+  }
+
+  @Test
+  void shouldNotRemoveRecommendationWithGivenIdIfNotPresent() {
+    when(recommendationRepository.findById(RECOMMENDATION_ID))
+        .thenReturn(Optional.empty());
+
+    changeLog.removeDuplicateDraftFor7449295(recommendationRepository);
+
+    verify(recommendationRepository, never()).delete(any());
+  }
+
+  private Recommendation buildTestRecommendation() {
+    return Recommendation.builder()
+        .id(RECOMMENDATION_ID)
+        .build();
+  }
+}
