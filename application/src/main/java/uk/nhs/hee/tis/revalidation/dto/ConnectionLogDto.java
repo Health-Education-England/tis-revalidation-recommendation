5b909b1e087509cb215a2a0552be150fd513b600
NM2.7JBY9KP8 <cai.willis@nhs.net>
2026-01-30 12:06:42 +0000

    fix: date formatting error for external gmc connection change logs
    TIS21-8251: ConnectionLog Updates to Elasticsearch from GMC being dropped due to dateformat error

--- application/src/main/java/uk/nhs/hee/tis/revalidation/dto/ConnectionLogDto.java
+++ application/src/main/java/uk/nhs/hee/tis/revalidation/dto/ConnectionLogDto.java
@@ -20,6 +20,7 @@
  */
 package uk.nhs.hee.tis.revalidation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModel;
 import java.time.LocalDateTime;
@@ -39,5 +40,6 @@ public class ConnectionLogDto {
   private String newDesignatedBodyCode;
   private String previousDesignatedBodyCode;
   private String updatedBy;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSS")
   private LocalDateTime eventDateTime;
 }
